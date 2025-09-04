2643b2842e460b8ead77ed94d9af6a081aedf9cc
Ronit Tamrakar <ronittmkr17@gmail.com>
2025-09-04 09:40:26 +0545

    FINAL C0MMIT

--- server.js
+++ server.js
@@ -13,3 +13,36 @@ app.get("/", (req, res)=> {
 app.listen(port, (r,rs)=>{
     console.log(`Server is running on http://localhost:${port}`);
 })
+
+let users = [
+    { id: 1, name: "Ram", email: "Ram@gmail.com" },
+    { id: 2, name: "Bob", email: "bob@gmail.com" }
+]; 
+
+app.post("/users/add", (req, res)=> {
+    const {name, email} = req.body;
+    const newUser = {
+        id: users.length + 1,
+        name,  
+        email
+    }
+    users.push(newUser);
+    res.status(201).json(newUser);
+})
+
+app.get("/users", (req, res)=> {
+    res.json(users);
+})  
+
+app.put("/users/update/:id", (req,res)=>{
+    const userId = parseInt(req.params.id);
+    const {name, email}= req.body;
+    const user = users.find(u => u.id === userId);
+
+    if(!user){
+        return res.status(404).json({message: "User not found"});
+    }
+    user.name = name || user.name;
+    user.email = email || user.email;
+    res.json(user);
+})
